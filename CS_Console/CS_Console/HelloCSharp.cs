f65d879826c629958d6c3db9941ca4067d4d1da6
JohnKingm123 <73373145+JohnKingm123@users.noreply.github.com>
2024-03-29 17:08:45 +0800

    example Rectangle
    Day 0

---
+++ CS_Console/CS_Console/HelloCSharp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using RectAPP;
+
+
+namespace Helloworld_Application {
+    class HelloWorld {
+        static void Main(string[] args) {
+            Console.WriteLine("HelloWorld");
+            Console.ReadKey();
+            Rect A = new Rect();
+            A.AcceptDetails();
+            A.OutPut();
+            Console.ReadLine();
+            //////////        A.AcceptDetails();
+            //////////        A.OutPut();
+            //////////        Console.ReadLine();
+
+
+        }
+
+    }
+
+}
---
+++ CS_Console/CS_Console/Rect.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace RectAPP
+{
+    class Rect {
+    double lenth;
+    double width;
+        
+    public void AcceptDetails() { 
+        lenth = 4.5;
+        width = 3.5;
+    }
+
+        public double GetS() {
+            return lenth * width;
+        }
+        public void OutPut() {
+            Console.WriteLine("Length:{0}",lenth);
+            Console.WriteLine("Width:{0}", width);
+            Console.WriteLine("Area:{0}", GetS());
+        }
+    }
+
+    //class RxecuteRectangle {
+    //    static void Main(string[] args) {
+    //        Rect A = new Rect();
+    //        A.AcceptDetails();
+    //        A.OutPut();
+    //        Console.ReadLine();
+        
+    //    }
+    
+    
+    
+    //}
+
+
+
+}
