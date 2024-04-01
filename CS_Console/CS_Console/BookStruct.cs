d18f83082b512f29d030f5208333834ae160dc0c
JohnKingm123 <73373145+JohnKingm123@users.noreply.github.com>
2024-04-01 12:57:41 +0800

    day 1

---
+++ CS_Console/CS_Console/BookStruct.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BookSturcts
+
+{
+    struct Book {
+        private string title;
+        private string author;
+        private string subject;
+        private int book_id;
+
+        public void setValue(string t, string a, string s, int  id) { 
+            title = t; author = a; subject = s; book_id = id;   
+        }
+
+        public void display() { 
+            Console.WriteLine(title);
+            Console.WriteLine(author);
+            Console.WriteLine(subject);
+            Console.WriteLine(book_id);
+            Console.WriteLine("");
+        }
+
+    }
+
+
+
+
+
+}
--- CS_Console/CS_Console/HelloCSharp.cs
+++ CS_Console/CS_Console/HelloCSharp.cs
@@ -1,15 +1,34 @@
 ﻿using System;
 using RectAPP;
+using BookSturcts;
 
 
 namespace Helloworld_Application {
+
+
+    struct MyStruct {
+        public int X; 
+        public int Y;
+
+        public MyStruct(int x , int  y){
+            X = x;
+            Y = y;
+        }    
+    }
+
+
     class HelloWorld {
         static void Main(string[] args) {
             Console.WriteLine("HelloWorld");
-            Console.ReadKey();
-            Rect A = new Rect();
-            A.AcceptDetails();
-            A.OutPut();
+            Book Book1 = new Book();
+            Book Book2 = new Book();
+
+            Book1.setValue("C Programming","Nuhua Ali", "C Programming Tutorial", 0);
+            Book2.setValue("CS Programming", "Nuhua Ali", "CS Programming Tutorial", 1);
+
+            Book1.display();
+            Book2.display();
+
             Console.ReadLine();
             //////////        A.AcceptDetails();
             //////////        A.OutPut();
