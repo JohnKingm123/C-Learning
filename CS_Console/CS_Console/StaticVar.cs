97dc2cd8ff340bf6ca61d9d93d6ce0c3b80b98a5
JohnKingm123 <73373145+JohnKingm123@users.noreply.github.com>
2024-04-01 21:59:31 +0800

    Day1-1
    use of "~" and static Vars

---
+++ CS_Console/CS_Console/StaticVar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statics
+
+{
+    class StaticVar
+    {
+        public static int num;
+        public void count()
+        {
+            num++;
+        }
+        public int getNum()
+        {
+            return num;
+        }
+    }
+    class StaticTester
+    {
+        static void Main(string[] args)
+        {
+            StaticVar s1 = new StaticVar();
+            StaticVar s2 = new StaticVar();
+            s1.count();
+            s1.count();
+            s1.count();
+            s2.count();
+            s2.count();
+            s2.count();
+            Console.WriteLine("s1 的变量 num： {0}", s1.getNum());
+            Console.WriteLine("s2 的变量 num： {0}", s2.getNum());
+            Console.ReadKey();
+        }
+    }
+}
